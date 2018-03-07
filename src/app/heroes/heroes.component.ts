616611060aab79e67c38fa50857e82fbca08b9f1
lukile <lucile.1988.ls@gmail.com>
2018-03-07 17:54:34 +0100

    Added dashboard to navigate between dashboard and heroes

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -10,7 +10,7 @@ import { HeroService } from '../hero.service';
 })
 export class HeroesComponent implements OnInit {
   selectedHero: Hero;
-  heroes = Hero;
+  heroes:  Hero[];
 
 
   constructor(private heroService: HeroService) {
