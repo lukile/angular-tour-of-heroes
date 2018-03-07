2bed11ef6bfc66fe5bda8631c6c1fd7bef0118a8
lukile <lucile.1988.ls@gmail.com>
2018-03-07 18:07:01 +0100

    Added some other routes

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -9,9 +9,7 @@ import { HeroService } from '../hero.service';
   styleUrls: ['./heroes.component.css'],
 })
 export class HeroesComponent implements OnInit {
-  selectedHero: Hero;
   heroes:  Hero[];
-
 
   constructor(private heroService: HeroService) {
   }
@@ -19,13 +17,7 @@ export class HeroesComponent implements OnInit {
     this.getHeroes();
   }
 
-  onselect(hero: Hero): void {
-    this.selectedHero = hero;
-  }
-
   getHeroes(): void {
     this.heroService.getHeroes().subscribe(heroes => this.heroes = heroes);
   }
-
 }
-
